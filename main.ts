c3fc8364bb464d786745610d5ecb1a631d3ec5c7
TahaNaeem1995 <160178535+TahaNaeem1995@users.noreply.github.com>
2024-04-20 16:48:58 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,55 @@
+import inquirer from "inquirer";
+
+let myBalance  = 10000; //Dollar
+let myPin  = 1234;
+
+
+
+let pinAnswer = await inquirer.prompt(
+   [
+    {
+       name:"pin",
+     message: "enter your pin",
+     type: "number"
+     }
+ ] 
+);
+//12345 === 1234 - false
+if (pinAnswer.pin === myPin){
+    console.log("correct pin code!!!");
+
+    let operationAns = await inquirer.prompt(
+      [
+        {
+          name:"operation",
+          message:"please select option",
+          type:"list",
+          choices: ["withdrawl", "check balance"]
+        }
+      ]
+    );
+
+    console.log(operationAns);
+
+  if (operationAns === "withdrawl") {
+    let amountAns = await inquirer.prompt(
+      [
+        {
+          name: "amount",
+          message: "enter your amount",
+          type: "number"
+        }
+      ]
+    );
+    // =, -=, +=
+    myBalance -= amountAns.amount;
+
+    console.log("your remaining balance is: " + myBalance)
+
+  } else if (operationAns.operation === "check balance"){
+    console.log("your balance is: + myBalance)")
+  }
+
+}
+
+
